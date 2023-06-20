6a6b666ee2cd23d2ab7f6d35b7f64601291f9add
afkan_guseynov <af.kan@bk.ru>
2023-06-20 18:40:01 +0400

    бинарный поиск

--- MyEx/src/BinarySearch.java
+++ MyEx/src/BinarySearch.java
@@ -1,47 +1,30 @@
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] myList = {87, 21, 45, 93};
-        System.out.println(binarySearch(myList, 23));
-        System.out.println(binarySearch(myList,12));
+        int[] myArray = {87, 21, 45, 93};
+        System.out.println(binarySearch(myArray, 23));
+        System.out.println(binarySearch(myArray, 12));
     }
 
-    public static int binarySearch(int[] list, int item) {
-        if (isListEmpty(list)) {
+    //если значение присутсвует в массиве,то вернуть его его позицию
+    public static int binarySearch(int[] array, int item) {
+        if (isArrayEmpty(array)) {
             return -1;
         }
         int low = 0;
-        int high = list.length - 1;
-
+        int high = array.length - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
-            int guess = list[mid];
-
-            if (guessEqualsIsItem(guess, item)) {
+            int guess = array[mid];
+            if (guessEqualsItem(guess, item)) {
                 return mid;
             } else if (guessGreaterThanItem(guess, item)) {
                 high = mid - 1;
             } else if (guessLessThanItem(guess, item)) {
-                low = mid + 1;
+               low = mid + 1;
             }
         }
         return -1;
-    }
-
-    public static boolean isListEmpty(int[] myList) {
-        int listSize = myList.length;
-        if (listSize == 0) {
-            return true;
-        }
-        return false;
-
-    }
-
-    public static boolean guessEqualsIsItem(int guess, int item) {
-        if (guess != item) {
-            return false;
-        }
-        return true;
     }
 
     public static boolean guessGreaterThanItem(int guess, int item) {
@@ -53,6 +36,22 @@ public class BinarySearch {
 
     public static boolean guessLessThanItem(int guess, int item) {
         if (guess > item) {
+            return false;
+        }
+        return true;
+    }
+
+    //пустой массив или нет
+    public static boolean isArrayEmpty(int[] myArray) {
+        int arraySize = myArray.length;
+        if (arraySize == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean guessEqualsItem(int guess, int item) {
+        if (guess != item) {
             return false;
         }
         return true;
